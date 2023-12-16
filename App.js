b135febe126fc71b1270301a1ad6707c6b1c78d1
johncarlorazon <143332543+darwinpatac@users.noreply.github.com>
2023-12-16 14:57:28 +0800

    INITAIL CHANGES

--- App.js
+++ App.js
@@ -1,20 +1,107 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, FlatList, TouchableOpacity, Modal, ScrollView, Button, Image } from 'react-native';
+import { styles } from './styles/styles';
 
-export default function App() {
+
+const userDataWithImages = [
+  {
+    firstName: 'Benji',
+    lastName: 'Dogoy',
+    nickname: 'benj',
+    course: 'BSCRIM',
+    year: 'Third Year',
+    
+  },
+  {
+    firstName: 'Aldren',
+    lastName: 'Banga',
+    nickname: 'Aden',
+    course: 'BSCRIM',
+    year: 'Third Year',
+    
+  },
+  {
+    firstName: 'Christian',
+    lastName: 'Camanzo',
+    nickname: 'Ikyan',
+    course: 'BSELECT',
+    year: 'Third Year',
+    
+  },
+  {
+    firstName: 'Robert',
+    lastName: 'Stampa',
+    nickname: 'vert',
+    course: 'BSCRIM',
+    year: 'Third Year',
+    
+  },
+  {
+    firstName: 'James',
+    lastName: 'Camanzo',
+    nickname: 'OPI',
+    course: 'BSCRIM',
+    year: 'Third Year',
+    
+  },
+  {
+    firstName: 'Darwin',
+    lastName: 'Patac',
+    nickname: 'Dar',
+    course: 'BSIT',
+    year: 'Third Year',
+
+  },
+];
+
+const UserList = () => {
+  const [selectedUser, setSelectedUser] = React.useState(null);
+  const [modalVisible, setModalVisible] = React.useState(false);
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => {
+      setSelectedUser(item);
+      setModalVisible(true);
+    }}>
+      <Text style={styles.nickname}>{item.nickname}</Text>
+    </TouchableOpacity>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+       <Text style={styles.pt}>DRINGKING MASTER</Text>
+      <FlatList
+        data={userDataWithImages}
+        keyExtractor={(item) => item.nickname}
+        renderItem={renderItem}
+        contentContainerStyle={styles.listContainer}
+      />
+
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.modalContainer}>
+          {selectedUser && (
+            <ScrollView>
+              <Text style={styles.fullName}>
+                {selectedUser.firstName} {selectedUser.lastName}
+              </Text>
+              <Text>Nickname: {selectedUser.nickname}</Text>
+              <Text>Course: {selectedUser.course}</Text>
+              <Text>Year: {selectedUser.year}</Text>
+              {selectedUser.image && <Image source={selectedUser.image} style={{ width: 200, height: 200 }} />}
+              <Button title="Close" onPress={() => setModalVisible(!modalVisible)} />
+            </ScrollView>
+          )}
+        </View>
+      </Modal>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default UserList;
---
+++ styles/styles.jsx
@@ -0,0 +1,40 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  listContainer: {
+    flexGrow: 1,
+    justifyContent: 'center',
+  },
+  nickname: {
+    fontSize: 30,
+    margin: 10,
+    color: 'black',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  fullName: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    textAlign: 'center',
+    color: 'violet',
+  },
+  pt:{
+    fontSize:40,
+    fontWeight:'bolder',
+    color: 'violet',
+  
+
+  }
+
+});
